986e76746f3b742951bc4d767f297a21daae1903
mobetav83610 <mobetav83610@gmail.com>
2019-06-03 23:42:07 +0800

    style and search modified

--- src/js/container/VisibleList.js
+++ src/js/container/VisibleList.js
@@ -3,21 +3,26 @@ import { connect } from 'react-redux';
 import TbContainer from './TbContainer'
 
 const getVisibleList = (formList, searchWord) => {
-  if (searchWord ==="") {
+  if (searchWord =="") {
     return formList;
   } else {
     let filteredList = [];
     formList.forEach((item)=>{
       for(let key in item){
-        if ((key==="seqno"||key==="name") && item[key].toString().toLowerCase().indexOf(searchWord)>=0) {
+        if ((key=="seqno"||key=="name") && item[key].toString().toLowerCase().indexOf(searchWord)>=0) {
           filteredList.push(item);
           return;
         }
-        if (key==="areaCode" && (item[key].toString()+item["phone"].toString()).toLowerCase().indexOf(searchWord)>=0) {
-          filteredList.push(item);
-          return;
+        if (key=="areaCode") {
+          let withAreaCode = item[key]==""?"":(item[key]+"-");
+          withAreaCode+=item["phone"].toString();
+          let withoutAreaCode = item[key].toString()+item["phone"].toString();
+          if (withAreaCode.indexOf(searchWord)>=0 || withoutAreaCode.indexOf(searchWord)>=0) {
+            filteredList.push(item);
+            return;
+          }
         }
-        if (key==="emailPrefix" && (item[key]+"@"+item["emailSuffix"]).toLowerCase().indexOf(searchWord)>=0) {
+        if (key=="emailPrefix" && (item[key]+"@"+item["emailSuffix"]).toLowerCase().indexOf(searchWord)>=0) {
           filteredList.push(item);
           return;
         }
